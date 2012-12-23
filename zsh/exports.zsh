7df19943a12db40637b751026d7b46b525fad936
patbenatar <nick@gophilosophie.com>
2012-12-23 15:27:16 -0800

    Projects instead of Sites

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,6 +1,6 @@
 export MANPATH=/opt/local/share/man:$MANPATH
 export PATH=$HOME/.pilot/bin:$HOME/bin:$HOME/.rbenv/bin:/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$PATH
-export CDPATH=~/Sites:~/
+export CDPATH=~/Projects:~/
 export RUBYOPT='rubygems'
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='3;33'
@@ -8,7 +8,7 @@ export EDITOR='subl -w'
 export TERM=xterm-color
 export LSCOLORS=gxfxcxdxbxegedabagacad
 export CLICOLOR=1
-export PROJECTS=$HOME/Sites
+export PROJECTS=$HOME/Projects
 export NODE_PATH="/usr/local/bin/node:/usr/local/lib/node_modules"
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 if which pilot > /dev/null; then eval "$(pilot init -)"; fi
