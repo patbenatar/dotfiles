affb9f11272b7ffecb4b0ef9eb1b88af07cd07f4
Nick Giancola <nick@philosophie.is>
2018-03-20 09:28:19 -0700

    Load nvm all the time

--- zsh/bootstrap.zsh
+++ zsh/bootstrap.zsh
@@ -1,5 +1,13 @@
+printf "Loading: rbenv... "
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
-function loadnvm {
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-}
+printf "virtualenv... "
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/projects
+source /usr/local/bin/virtualenvwrapper.sh
+
+echo "nvm..."
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
